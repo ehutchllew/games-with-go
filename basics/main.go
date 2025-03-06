0878cb2d12d5af565b813426e841de976e401647
Evan <ehutch7@gmail.com>
2025-03-06 02:29:32 +0000

    Added some extra checks against the low and high bounds; error handling on strconv of input to num; and outputting total guesses to win

--- basics/main.go
+++ basics/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -12,10 +14,26 @@ func main() {
 	low := 1
 	high := 100
 
-	fmt.Println("Please think of a number between", low, "and", high)
-	fmt.Println("Press ENTER when ready")
-	scanner.Scan()
+	for {
+		fmt.Println("Please think of a number between", low, "and", high)
+		fmt.Println("Press ENTER when ready")
+		scanner.Scan()
 
+		num, err := strconv.ParseInt(scanner.Text(), 10, 64)
+		if err != nil {
+			log.Fatalf("Tried to parse input value, err: %v", err)
+		}
+
+		numCast := int(num)
+
+		if numCast < low || numCast > high {
+			fmt.Println("Number out of range, try again.")
+			continue
+		}
+		break
+	}
+
+	numGuesses := 1
 	for {
 		// Binary search log(n)
 		guess := (low + high) / 2
@@ -29,10 +47,12 @@ func main() {
 
 		if response == "a" {
 			high = guess - 1
+			numGuesses++
 		} else if response == "b" {
 			low = guess + 1
+			numGuesses++
 		} else if response == "c" {
-			fmt.Println("I won!")
+			fmt.Printf("I won in %d turns!\n", numGuesses)
 			break
 		} else {
 			fmt.Println("Invalid response, try again")
